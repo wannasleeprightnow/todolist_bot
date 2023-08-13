f19dc61795f9f363bf3aa1b458d936aa14ffc554
enoughtless <enoughtless@gmail.com>
2023-08-13 20:04:18 +0500

    fixes and add output

--- main.py
+++ main.py
@@ -21,10 +21,15 @@ async def add_task(message):
     user_id = message.chat.id
     async with aiosqlite.connect("db.sqlite") as db:
         await db.execute(f"""INSERT INTO tasks
-(user_id, task_text, planned_date, added_at, finished_at)
-VALUES({user_id}, '{task_text}', '{planned_date}', current_timestamp, null);
+(user_id, task_text, planned_date, added_at)
+VALUES({user_id}, '{task_text.capitalize()}', '{planned_date}', current_timestamp);
 """, )
         await db.commit()
+    await bot.reply_to(
+        message,
+        f'Ваше дело "{task_text.capitalize()}" запланировано на {planned_date}.'
+        )
+
 
 
 @bot.message_handler(commands=["view"])
@@ -33,11 +38,18 @@ async def view_day(message):
     user_id = message.chat.id
     
     async with aiosqlite.connect("db.sqlite") as db:
-        async with db.execute(f"""SELECT task_text 
+        async with db.execute(f"""SELECT task_text, finished_at
 FROM tasks
 WHERE user_id = {user_id} AND planned_date LIKE '%{date}%'""") as cursor:
-            days_plans = [plan[0] async for plan in cursor]
-    await bot.reply_to(message, "\n".join([f"{number + 1}. {plan}" for number, plan in enumerate(days_plans)]))
+            days_plans = [plan async for plan in cursor]
+            print(days_plans)
+            plans = []
+            for number, plan in enumerate(days_plans):
+                plans.append(f'{number + 1}. {plan[0].capitalize()}.{"" if plan[1] is None else "(выполнено)"}')
+    plans = "\n".join(plans)
+    await bot.reply_to(
+        message, 
+        f'Вот дела, которые Вы запланировали на {date}:\n{plans}')
 
 
 @bot.message_handler(commands=["delete"])
@@ -50,11 +62,10 @@ async def delete_task(message):
 FROM tasks
 WHERE user_id = {user_id} AND planned_date LIKE '%{date}%'""") as cursor:
             task_text = [plan[0] async for plan in cursor][int(task_number)]
-    
-    async with aiosqlite.connect("db.sqlite") as db:
         await db.execute(f"""DELETE FROM tasks
 WHERE user_id = {user_id} AND task_text = '{task_text}'""")
         await db.commit()
+    await bot.reply_to(message, f'"{task_text.capitalize()}" был удален из Ваших планов на {date}.')
 
 
 @bot.message_handler(commands=["finish"])
@@ -66,13 +77,15 @@ async def finish_task(message):
         async with db.execute(f"""SELECT task_text 
 FROM tasks
 WHERE user_id = {user_id} AND planned_date LIKE '%{date}%'""") as cursor:
-            task_text = [plan[0] async for plan in cursor][int(task_number)]
+            task_text = [plan[0] async for plan in cursor][int(task_number) - 1]
+            print(task_text)
     
     async with aiosqlite.connect("db.sqlite") as db:
         await db.execute(f"""UPDATE tasks
 SET finished_at = current_timestamp
 WHERE user_id = {user_id} AND task_text = '{task_text}'""")
         await db.commit()
+    await bot.reply_to(message, f'"{task_text.capitalize()}" завершён!')
     
     
     
